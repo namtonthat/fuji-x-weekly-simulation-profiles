d1c25b81c865eb3f5eb0c7dae057191bbc7f700c
Nam Tonthat <n.nam.tonthat@gmail.com>
2023-11-18 09:19:59 +1100

    fix setup.sh

--- local_setup/setup.sh
+++ local_setup/setup.sh
@@ -1,12 +1,12 @@
 ## Setup script for local development
 export PYTHON_VERSION=3.11.6
 
-@echo "🚀 Creating virtual environment using pyenv and poetry"
+echo "🚀 Creating virtual environment using pyenv and poetry"
 # Install Python
 pyenv install $PYTHON_VERSION
 pyenv virtualenv $PYTHON_VERSION fuji-x-weekly-simluation-profiles-$PYTHON_VERSION
 
 # Install dependencies
-poetry install
+poetry install --no-root
 poetry run pre-commit install
 poetry shell
